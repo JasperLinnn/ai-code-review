2a39b378acb7c3fb2de1d125173c1fb462d004d6
紫红 <zihong@micous.com>
2025-03-25 11:59:04 +0800

    feat: v2

--- ai-code-review-sdk/src/main/java/com/jasper/sdk/AiCodeReview.java
+++ ai-code-review-sdk/src/main/java/com/jasper/sdk/AiCodeReview.java
@@ -1,11 +1,35 @@
 package com.jasper.sdk;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+
 /**
  * @author: zihong@micous.com
  * @date: 2025/3/24 16:10
  **/
+@Slf4j
 public class AiCodeReview {
     public static void main(String[] args) {
-        System.out.println("测试执行");
+        ProcessBuilder pb = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        pb.directory(new File("."));
+        try {
+            Process p = pb.start();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            StringBuilder sb = new StringBuilder();
+            while((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            int exitCode = p.waitFor();
+            System.out.println("Exited with code:" + exitCode);
+            System.out.println("评审代码:" + sb.toString());
+
+        } catch (Exception e) {
+            log.error("git diff error", e);
+        }
     }
 }
